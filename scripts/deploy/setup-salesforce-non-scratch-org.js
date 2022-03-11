13c14f923552f65600b4f6e64d8ec2cbc14e765c
Mohith Shrivastava <mohith.shrivastava@salesforce.com>
2022-03-10 20:36:22 -0500

    fix: deploy script bug

--- scripts/deploy/setup-salesforce-non-scratch-org.js
+++ scripts/deploy/setup-salesforce-non-scratch-org.js
@@ -6,10 +6,21 @@ const { assignPermissionset, loadSampleData } = require('./util');
 
 // Push source to scratch org, apply permset, and load sample data
 const setupDefaultNonScratchOrg = async () => {
+    log('');
+    log(
+        `${chalk.bold('*** Setting up Salesforce App')} ${chalk.dim(
+            '(step 1 of 2)'
+        )}`
+    );
+    const userData = JSON.parse(
+        sh.exec('sfdx force:org:display --json', { silent: true })
+    );
+    sh.env.SF_USERNAME = userData.result.username;
+    sh.env.ORGID = userData.result.id;
     log('*** Deploying Salesforce metadata');
     const deployResult = JSON.parse(
         sh.exec(
-            `sfdx force:source:push -u ${sh.env.SF_USERNAME} -w 10 --json`,
+            `sfdx force:source:deploy -u ${sh.env.SF_USERNAME} -p force-app/main/default -w 10 --json`,
             { silent: true }
         )
     );
@@ -19,7 +30,6 @@ const setupDefaultNonScratchOrg = async () => {
             'Source deployment failed ' + JSON.stringify(deployResult)
         );
     }
-
     // Assign permission set to user
     await assignPermissionset();
     // Load sample data
