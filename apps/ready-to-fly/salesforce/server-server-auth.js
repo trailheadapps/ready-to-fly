9fda94c06e2958a3d581b51c373f3e0815d62ba9
Alba Rivas <arivas@salesforce.com>
2022-03-18 12:17:06 +0100

    Debugging errors

--- apps/ready-to-fly/salesforce/server-server-auth.js
+++ apps/ready-to-fly/salesforce/server-server-auth.js
@@ -9,31 +9,31 @@ class ServerToServerAuth {
     }
 
     async connect() {
-        try {
-            // jwt-bearer flow
-            this.conn = new jsforce.Connection({
-                version: this.config.apiVersion
-            });
+        //try {
+        // jwt-bearer flow
+        this.conn = new jsforce.Connection({
+            version: this.config.apiVersion
+        });
 
-            // Get JWT Token
-            const jwtResponse = await getToken({
-                iss: this.config.clientId,
-                sub: this.config.username,
-                aud: this.config.loginUrl,
-                privateKey: this.config.privateKey
-            });
+        // Get JWT Token
+        const jwtResponse = await getToken({
+            iss: this.config.clientId,
+            sub: this.config.username,
+            aud: this.config.loginUrl,
+            privateKey: this.config.privateKey
+        });
 
-            // Initialize connection
-            this.conn.initialize({
-                instanceUrl: jwtResponse.instance_url,
-                accessToken: jwtResponse.access_token
-            });
-            return this.conn;
-        } catch (e) {
+        // Initialize connection
+        this.conn.initialize({
+            instanceUrl: jwtResponse.instance_url,
+            accessToken: jwtResponse.access_token
+        });
+        return this.conn;
+        /*} catch (e) {
             throw new Error(
                 `Can't establish connection with Salesforce, reason: ${e.message}`
             );
-        }
+        }*/
     }
 }
 
