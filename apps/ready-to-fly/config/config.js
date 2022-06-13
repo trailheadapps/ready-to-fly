308872141146abbb2bacc28d0c88fe108b976de5
Alba Rivas <arivas@salesforce.com>
2022-06-13 07:01:07 +0100

    Upgrade API version (#57)

--- apps/ready-to-fly/config/config.js
+++ apps/ready-to-fly/config/config.js
@@ -21,7 +21,7 @@ requiredEnvVars.forEach((envVar) => {
     }
 });
 
-const defaultSalesforceApiVersion = '54.0';
+const defaultSalesforceApiVersion = '55.0';
 
 const privateKeyBase64Decoded = Buffer.from(
     process.env.PRIVATE_KEY,
